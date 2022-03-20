31ad0774186cb42c2ee558d68552cad9f71a4f49
Jota30200201 <101662254+Jota30200201@users.noreply.github.com>
2022-03-20 18:27:20 +0000

    Classe Venda feita

--- src/main/java/Exercicio6/Venda.java
+++ src/main/java/Exercicio6/Venda.java
@@ -1,10 +1,55 @@
 package Exercicio6;
 
 import java.util.Date;
-
+import java.util.List;
 
 public class Venda {
     int numVenda;
     Date dataVenda;
     float valor;
-}
+    List<Artigos> artigos;
+    
+    
+    /**seter da variável valor
+     *@param valor
+    */
+    
+    
+        public void setValor(float valor){
+        this.valor = valor;
+        }
+        
+        
+        
+    /** seter da variável dataVenda
+        * @param dt 
+     */
+        
+        
+        public void setData(Date dt){
+        this.dataVenda = dt;
+        }
+        
+        
+        
+     /** geter da variável valor
+      * 
+      * @return 
+      */   
+        
+        
+        public float getValor(){
+        return valor;
+        }
+        
+     /** geter da variável dataVenda
+      * 
+      * @return 
+      */ 
+        
+        
+        public Date getData(){
+        return dataVenda;
+        }
+   }
+

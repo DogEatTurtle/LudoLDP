da6f024b2a7acbdccc1bdeeaa109e1f3de7d5800
DogEatTurtle <brunomiguelduarte27@hotmail.com>
2022-04-07 18:27:24 +0100

    Branch do Bruno criado

--- src/main/java/Experimentar/LudoLDP.java
+++ src/main/java/Experimentar/LudoLDP.java
@@ -13,5 +13,7 @@ public class LudoLDP {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Branch do Bruno criado");
     }
+    
 }
